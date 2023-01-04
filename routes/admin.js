d5bb0fbcec2ed31af1ecb4349728f7a8d3f97062
suhail th <suhailth17756@gmail.com>
2023-01-04 10:33:50 +0530

    session added

--- routes/admin.js
+++ routes/admin.js
@@ -9,31 +9,45 @@ adminrouter.use(express.json())
 
 
 adminrouter.get('/',adminController.getlogin)
+
 adminrouter.post('/adminhome',adminController.postlogin)
+
 adminrouter.get('/adminhome',adminController.getadminhome)
+
 adminrouter.get("/adminlogout",adminController.adminlogout)
 
 
 // ****************************product*****************************************
-adminrouter.get('/addproduct',adminController.getaddproduct)
-adminrouter.post('/addproduct',adminController.postaddproduct)
-adminrouter.get('/deleteproduct/:id',adminController.deleteproduct)
-adminrouter.get('/viewproduct',adminController.getviewproduct)
-adminrouter.get("/editproduct/:id",adminController.editproduct)
-adminrouter.post('/editproduct/:id',adminController.addeditproduct)
+adminrouter.get('/addproduct',verifyLogin.verifyLoginAdmin, adminController.getaddproduct)
+
+adminrouter.post('/addproduct',verifyLogin.verifyLoginAdmin,adminController.postaddproduct)
+
+adminrouter.get('/deleteproduct/:id',verifyLogin.verifyLoginAdmin,adminController.deleteproduct) 
+
+adminrouter.get('/viewproduct',verifyLogin.verifyLoginAdmin,adminController.getviewproduct)
+
+adminrouter.get("/editproduct/:id",verifyLogin.verifyLoginAdmin,adminController.editproduct)
+
+adminrouter.post('/editproduct/:id',verifyLogin.verifyLoginAdmin,adminController.addeditproduct)
 
 // **********************************usermangement*****************************
-adminrouter.get('/viewusers',adminController.getviewusers)
-adminrouter.get('/blockuser/:id',adminController.blockuser)
-adminrouter.get('/unblockuser/:id',adminController.unblockuser)
+adminrouter.get('/viewusers',verifyLogin.verifyLoginAdmin,adminController.getviewusers)
+
+adminrouter.get('/blockuser/:id',verifyLogin.verifyLoginAdmin,adminController.blockuser)
+
+adminrouter.get('/unblockuser/:id',verifyLogin.verifyLoginAdmin,adminController.unblockuser)
 
 
 // **************************************category**********************************
-adminrouter.get('/viewcategory',adminController.viewcategory)
-adminrouter.get("/addcategory",adminController.addcategory)
-adminrouter.post("/addcategory",adminController.postaddcategory)
-adminrouter.get("/deletecategory/:id",adminController.deletecategory)
-adminrouter.get("/backaddcategory",adminController.backaddcategory)
+adminrouter.get('/viewcategory',verifyLogin.verifyLoginAdmin,adminController.viewcategory)
+
+adminrouter.get("/addcategory",verifyLogin.verifyLoginAdmin,adminController.addcategory)
+
+adminrouter.post("/addcategory",verifyLogin.verifyLoginAdmin,adminController.postaddcategory)
+
+adminrouter.get("/deletecategory/:id",verifyLogin.verifyLoginAdmin,adminController.deletecategory)
+
+adminrouter.get("/backaddcategory",verifyLogin.verifyLoginAdmin,adminController.backaddcategory)
 
 
 
