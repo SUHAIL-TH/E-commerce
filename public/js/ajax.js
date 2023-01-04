a5ec9fb269a3787fce9b133397b9455f8ad07e32
suhail th <suhailth17756@gmail.com>
2023-01-04 10:33:03 +0530

    cart bug fixed

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,3 +1,5 @@
+const { response } = require("express")
+
 function addTocart(proId){
     $.ajax({
         url:'/addtocart/'+proId,
@@ -7,7 +9,54 @@ function addTocart(proId){
                 let count=$('#cartcount').html()
                 count=parseInt(count)+1,
                 $("#cartcount").html(count)
+                location.reload()
 
+            }
+        }
+    })
+}
+
+
+
+function changeQuantitty(cartId, proId, count) {
+    let quantity=parseInt(document.getElementById(proId).innerHTML)
+    count=parseInt(count)
+
+
+    $.ajax({
+        url: '/changeproductquantity',
+        data: {
+            cart: cartId,
+            product: proId,
+            count: count,
+            quantity:quantity
+        },
+        method:'post',
+        success: (response)=>{
+            if(response.removeProduct){
+                alert('product removed from cart')
+                location.reload()
+            }else{
+                document.getElementById(proId).innerHTML=quantity+count
+            }
+            
+        }
+    })
+}
+
+function removeProduct(cartId,proId) {
+    $.ajax({
+        url: '/removecartproduct',
+        data: {
+            cart: cartId,
+            product: proId
+
+        },
+        method: 'post',
+        success: (response) => {
+            if (response.removeProduct) {
+                alert('product removed from cart')
+                location.reload()
             }
         }
     })
